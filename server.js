0952e169f5892e91f73fc49e87e308b6e4eb68bf
Jennifer Winston <winston.jennifer9@gmail.com>
2016-11-01 16:45:08 -0400

    added models

---
+++ server.js
@@ -0,0 +1,102 @@
+// dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+// Our scraping tools are prepared, too
+var request = require('request'); 
+var cheerio = require('cheerio');
+
+// 'https://www.entrepreneur.com/topic/coding'
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/mongoScrape');
+var db = mongoose.connection;
+
+// show any errors
+db.on('error', function(err){
+	console.log('Mongoose error: ' + err);
+});
+
+// two models
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+
+
+// Routes
+app.get('/', function(req, res){
+	res.send(index.html);
+});
+
+// get request to scrape
+app.get('/scrape', function(req, res){
+	request('https://www.entrepreneur.com/topic/coding', function(error, response, html){
+		var $ = cheerio.load(html);
+
+		$('article h2').each(function(i, element){
+			var result = {};
+			// add text and save
+			result.title = $(this).children('a').text();
+			result.link = $(this).children('a').attr('href');
+
+			// create new entry
+			var entry = new Article (result);
+			entry.save(function(err, doc){
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(doc);
+				}
+			});
+
+		});
+	});
+
+	res.send("Scrape Complete");
+});
+
+// grabs an article
+app.get('/articles/:id', function (req, res){
+	Article.findOne({'_id': req.params.id})
+	.populate('note')
+	.exec(function(err, doc){
+		if(err){
+			console.log(err)
+		} else { res.json(doc) }
+	});
+});
+
+// replace existing note with new
+app.post('/articles/:id', function(req, res){
+	var newNote = new Note(req.body);
+
+	newNote.save(function(err, doc){
+		if (err) {
+			console.log(err);
+		} else {
+
+			Article.findOneAndUpdate({'_id': req.params.id}, {'note': doc._id})
+			.exec(function(err, doc){
+				if(err){
+					console.log(err);
+				} else { res.send(doc); }
+			});
+		}
+	});
+});
+
+//port listen
+app.listen(3000, function(){
+	console.log('App is running on port 3000');
+});
+
+
+
+
