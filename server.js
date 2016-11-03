7499b49e45083e58d45a12e7fe9b1b4ba025527c
Jennifer Winston <winston.jennifer9@gmail.com>
2016-11-03 10:42:09 -0400

    heroku3

--- server.js
+++ server.js
@@ -108,10 +108,10 @@ app.post('/articles/:id', function(req, res){
 });
 
 //port listen
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function (){
-	console.log("Listening on " + PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
 
 
 
